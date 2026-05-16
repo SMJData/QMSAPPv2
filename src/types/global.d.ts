b92facda1dcad8c399fc26903ef2991a4108f0c3
woodstaranalytic-cpu <woodstaranalytic@gmail.com>
2026-05-16 09:31:40 +0000

    fix font issut

---
+++ src/types/global.d.ts
@@ -0,0 +1,8 @@
+// Allow importing CSS files (including side-effect global imports) in TypeScript
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.less';
+declare module '*.styl';
+declare module '*.module.css';
+declare module '*.module.scss';
